9d3926104bb6d6932eac2c511f36530e70ab4f61
Fernando Dodino <fernando.dodino@gmail.com>
2014-06-18 15:26:07 +0000

    Refactor de factorial, eliminamos mostrarResultado que no se usaba y mejoramos nombres

--- factorial.js
+++ factorial.js
@@ -1,37 +1,23 @@
-function factorial(n, anteExito, anteFalla) {
+function factorialTryCatch(n, anteExito, anteFalla) {
 	if (n < 0)
 		anteFalla("n es " + n + " , n debe ser > 0");
 	else if (n == 0)
 		anteExito(1);
 	else
-		factorial(n - 1, function(t0) {
+		factorialTryCatch(n - 1, function(t0) {
 			anteExito(n * t0);
 		}, anteFalla);
 }
 
-function total_fact(n, anteExito) {
-	factorial(n, anteExito, function(ex) {
+function doFactorial(n, anteExito) {
+	factorialTryCatch(n, anteExito, function(ex) {
 		document.getElementById("mensaje").innerHTML = "";
 		alert("Llamaste mal a factorial porque " + ex);
 	});
 }
 
-function factorialOk() {
-	total_fact(5, function(res) {
-		document.getElementById("mensaje").innerHTML = "Factorial de 5 es: "
-				+ res
+function factorial(valor) {
+	doFactorial(valor, function(res) {
+		document.getElementById("mensaje").innerHTML = "Factorial de " + valor + " es: " + res
 	});
-}
-
-function factorialConError() {
-	total_fact(-1, function(res) {
-		document.getElementById("mensaje").innerHTML = "Factorial de -1 es: "
-				+ res
-	});
-}
-
-function mostrarResultado(res) {
-	return function(res) {
-		document.getElementById("mensaje").innerHTML = "Factorial es: " + res
-	}
 }

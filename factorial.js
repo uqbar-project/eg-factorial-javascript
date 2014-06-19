c557dba1ed22b922d2d868b3036256e9bfd05146
Fernando Dodino <fernando.dodino@gmail.com>
2014-06-19 11:45:28 +0000

    Refactor para simplificar las indirecciones que tenía

--- factorial.js
+++ factorial.js
@@ -1,3 +1,5 @@
+/* TODO Cambiar el ejemplo para que no use Factoriaaaaall!!!!
+ **/
 function factorialTryCatch(n, anteExito, anteFalla) {
 	if (n < 0)
 		anteFalla("n es " + n + " , n debe ser > 0");
@@ -9,15 +11,12 @@ function factorialTryCatch(n, anteExito, anteFalla) {
 		}, anteFalla);
 }
 
-function doFactorial(n, anteExito) {
-	factorialTryCatch(n, anteExito, function(ex) {
-		document.getElementById("mensaje").innerHTML = "";
-		alert("Llamaste mal a factorial porque " + ex);
-	});
-}
-
 function factorial(valor) {
-	doFactorial(valor, function(res) {
-		document.getElementById("mensaje").innerHTML = "Factorial de " + valor + " es: " + res
-	});
+	factorialTryCatch(valor, 
+		function(res) {
+			document.getElementById("mensaje").innerHTML = "Factorial de " + valor + " es: " + res
+		}, function(ex) {
+			document.getElementById("mensaje").innerHTML = "";
+			alert("Llamaste mal a factorial porque " + ex);
+		});
 }
